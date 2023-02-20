5221e15855ff30cbff24b99b1547d4b003c5fd97
Siqi <siqxiang@gmail.com>
2023-02-20 18:01:04 +0800

    Initial commit

---
+++ SUTDTimePlannerBackend/src/main/java/com/tsukisky/sutdtimeplannerbackend/SutdTimePlannerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tsukisky.sutdtimeplannerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SutdTimePlannerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SutdTimePlannerBackendApplication.class, args);
+    }
+
+}
---
+++ SUTDTimePlannerBackend/src/test/java/com/tsukisky/sutdtimeplannerbackend/SutdTimePlannerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsukisky.sutdtimeplannerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SutdTimePlannerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
