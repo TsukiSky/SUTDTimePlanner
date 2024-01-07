6ad00771505977116edf2d404c1f3182370be1f5
wangyanbao666 <yanbao_wang@mymail.sutd.edu.sg>
2024-01-07 16:03:27 +0800

    test: cors

--- SUTDTimePlannerBackend/src/main/java/com/tsukisky/sutdtimeplannerbackend/controller/CourseController.java
+++ SUTDTimePlannerBackend/src/main/java/com/tsukisky/sutdtimeplannerbackend/controller/CourseController.java
@@ -11,7 +11,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/course")
-@CrossOrigin(origins = "*")
 public class CourseController {
     private final CourseService courseService;
 
--- SUTDTimePlannerBackend/src/main/java/com/tsukisky/sutdtimeplannerbackend/controller/UserController.java
+++ SUTDTimePlannerBackend/src/main/java/com/tsukisky/sutdtimeplannerbackend/controller/UserController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
-@CrossOrigin(origins = "*")
 public class UserController {
     @Resource
     private UserService userService;
