557ce8e751c0b382b77f6078c88c19f1957e1964
wangyanbao666 <yanbao_wang@mymail.sutd.edu.sg>
2024-01-11 20:18:30 +0800

    fix: login page

--- SUTDTimePlannerFrontend/src/environments/environment.ts
+++ SUTDTimePlannerFrontend/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // apiUrl: 'http://54.255.209.55',
-  apiUrl: 'http://54.255.209.55'
+  apiUrl: 'http://localhost:8080',
+  // apiUrl: 'http://54.255.209.55/api'
 };
 
 /*
